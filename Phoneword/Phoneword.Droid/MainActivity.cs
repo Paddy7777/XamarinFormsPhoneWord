f92d5e36fa60092aae286486bb78db8ae3a6aa33
Patricio Porto <portopatricio@hotmail.com>
2016-03-05 18:04:23 +0100

    Subiendo el proyecto

---
+++ Phoneword/Phoneword.Droid/MainActivity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Content.PM;
+
+namespace Phoneword.Droid
+{
+    [Activity(Label = "Phoneword", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
+}
+
+
---
+++ Phoneword/Phoneword.WinPhone/PhoneDialer.cs
@@ -0,0 +1,26 @@
+﻿//using Microsoft.Phone.Tasks;
+using Phoneword.WinPhone;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(PhoneDialer))]
+
+namespace Phoneword.WinPhone
+{
+    public class PhoneDialer : IDialer
+    {
+        public bool Dial(string number)
+        {
+            //PhoneCallTask phoneCallTask = new PhoneCallTask
+            //{
+            //    PhoneNumber = number,
+            //    DisplayName = "Phoneword"
+            //};
+
+            //phoneCallTask.Show();
+
+            Windows.ApplicationModel.Calls.PhoneCallManager.ShowPhoneCallUI(number, $"{number} display name");
+
+            return true;
+        }
+    }
+}
